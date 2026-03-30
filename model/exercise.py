d24465e8bc85d96ee111e220025f65919ea76f16
Chirayu3108 <chirayu.ahuja3108@gmail.com>
2026-03-30 09:50:06 +0530

    Created Basic Structure for the App and Created SQL model for User

---
+++ model/exercise.py
@@ -0,0 +1,3 @@
+class Exercise:
+    def __init__(self, exercise_name, sets = None, reps = None, weight = None):
+        self.exercise_name = exercise_name 
---
+++ model/workout.py
@@ -0,0 +1,4 @@
+class Workout:
+    def __init__(self, workout_name):
+        self.workout_name = workout_name
+        self.exercise = []
